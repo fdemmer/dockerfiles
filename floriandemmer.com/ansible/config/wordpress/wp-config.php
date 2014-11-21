752ef6688e447438ac476200a483a65d36414b5a
Florian Demmer <fdemmer@gmail.com>
2014-11-21 14:13:50 +0100

    + move web root to /var/www and update wordpress config

--- floriandemmer.com/ansible/config/wordpress/wp-config.php
+++ floriandemmer.com/ansible/config/wordpress/wp-config.php
@@ -33,6 +33,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define('WP_HOME', 'https://{{ domain }}/blog');
+define('WP_SITEURL', 'https://{{ domain }}/blog');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
